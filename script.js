18e2805b4884bd3b895e2342b40d9642fe6df7b2
Atharv Lingayat <76042372+atharv2608@users.noreply.github.com>
2022-11-12 14:42:10 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -54,3 +54,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
   })
 })
 
+
+//Navbar
+const nav = document.querySelector('nav');
+		let prevScrollpos = window.pageYOffset;
+
+		window.addEventListener('scroll', ()=>{
+			let currentScrollPos = window.pageYOffset;
+
+			if(prevScrollpos < currentScrollPos){
+				nav.classList.add('hide');
+			}else{
+				nav.classList.remove('hide');
+			}
+
+			prevScrollpos = currentScrollPos;
+		})
